89f8ff4cc44c90d90fc24510de57395f997b1c90
Chester <chesterdeveloper2000@gmail.com>
2020-12-20 14:37:14 +0800

    intro to styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+// Global Style
+import GlobalStyle from "./components/GlobalStyle";
 // import pages
-import AboutUs from "./pages/AboutUs"
+import AboutUs from "./pages/AboutUs";
 
 function App() {
   return (
     <div className="App">
-     <AboutUs/>
+      <GlobalStyle />
+      <AboutUs />
     </div>
   );
 }
