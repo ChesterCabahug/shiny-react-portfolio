fa5b7d169db0f68add32ce9cb3b17b8037f25394
Chester <chesterdeveloper2000@gmail.com>
2020-12-20 15:03:38 +0800

    6. styling services section

--- src/components/GlobalStyle.jsx
+++ src/components/GlobalStyle.jsx
@@ -9,6 +9,7 @@ const GlobalStyle = createGlobalStyle`
 
 body {
 	background: #1b1b1b;
+    font-family: "Inter", san-serif;
 }
 h2 {
 	font-weight: lighter;
@@ -48,6 +49,7 @@ button {
 	background: transparent;
 	color: white;
 	transition: all 0.5s ease;
+    font-family: "Inter", san-serif;
 	&:hover{
 		background-color: #23d997;
 		color: white;
