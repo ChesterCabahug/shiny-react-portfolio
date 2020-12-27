046c1c6087ff528894fe326d9e17c4efb5fe1c05
Chester <chesterdeveloper2000@gmail.com>
2020-12-27 11:48:17 +0800

    testing framer motion �

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -1,26 +1,51 @@
 import home1 from "../img/home1.png";
-// styled
-import styled from "styled-components";
 
-import {About, Description, Image, Hide} from "../styles"
+import { About, Description, Image, Hide } from "../styles";
+
+// framer motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  const titleAnim = {
+    hidden: {
+      opacity: 0,
+    },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const container = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 1,
+        // when: "afterChildren",
+      },
+    },
+  };
   return (
     <About>
       <Description className="description">
-        <div className="title">
+        <motion.div
+          variants={container}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
@@ -35,6 +60,5 @@ const AboutSection = () => {
 };
 
 // styled components
-
 
 export default AboutSection;
